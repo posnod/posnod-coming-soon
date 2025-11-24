991d3fb7b6a77c041026ca7c2df768136639ec86
rikchooo <hermawanrikcho@gmail.com>
2025-11-24 11:42:13 +0700

    add sitemap and robots.txt, fix metadata and update project

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://posnod.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1.0,
+    }
+  ];
+}
