86a22d9f69b85300882b348196bbb965af24a513
rikchooo <hermawanrikcho@gmail.com>
2025-11-25 14:24:07 +0700

    update

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,6 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
       { url: '/faviconn-192.png', sizes: '192x192', type: 'image/png' },
       { url: '/faviconn-512.png', sizes: '512x512', type: 'image/png' }
     ],
